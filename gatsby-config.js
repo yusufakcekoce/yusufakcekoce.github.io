abb08a311a8eadf1b11f7fee17eb0dcb58a799c7
Kayra <kayrabtuncer@gmail.com>
2021-03-04 21:40:20 +0300

    Deploy gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,13 +118,10 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
