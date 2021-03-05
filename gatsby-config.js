e038f2276e5465e881ce098440bc37f0d7493a6f
Kayra <kayrabtuncer@gmail.com>
2021-03-05 17:34:19 +0300

    Dark mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-image`,
+    'gatsby-plugin-dark-mode',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
-import * as React from "react"
+import React from 'react'
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 const Footer = () => {
 
@@ -9,6 +10,19 @@ const Footer = () => {
             <a href="https://twitter.com/home">Twitter</a>
             {` `}
             <a href="https://www.linkedin.com/in/kayraberktuncer/">LinkedIn</a>
+
+            <ThemeToggler>
+                {({ theme, toggleTheme }) => (
+                    <label style={{ float: 'right' }}>
+                        <input
+                            type="checkbox"
+                            onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+                            checked={theme === 'dark'}
+                        />{' '}
+                        Dark mode
+                    </label>
+                )}
+            </ThemeToggler>
         </footer>
     )
 }
