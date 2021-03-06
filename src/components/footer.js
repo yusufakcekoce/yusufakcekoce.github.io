fa783ff159b1083e683b95d2fa7165f139bc7770
Kayra <kayrabtuncer@gmail.com>
2021-03-06 15:11:25 +0300

    footer design

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,15 +5,9 @@ const Footer = () => {
 
     return (
         <footer>
-            <a href="https://github.com/kayraberktuncer">Github</a>
-            {` `}
-            <a href="https://twitter.com/home">Twitter</a>
-            {` `}
-            <a href="https://www.linkedin.com/in/kayraberktuncer/">LinkedIn</a>
-
             <ThemeToggler>
                 {({ theme, toggleTheme }) => (
-                    <label style={{ float: 'right' }}>
+                    <label style={{ float: 'left' }}>
                         <input
                             type="checkbox"
                             onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
@@ -23,6 +17,13 @@ const Footer = () => {
                     </label>
                 )}
             </ThemeToggler>
+            <div className="social-links" style={{ float: 'right' }}>
+                <a href="https://github.com/kayraberktuncer">Github</a>
+                {` `}
+                <a href="https://twitter.com/home">Twitter</a>
+                {` `}
+                <a href="https://www.linkedin.com/in/kayraberktuncer/">LinkedIn</a>
+            </div>
         </footer>
     )
 }
