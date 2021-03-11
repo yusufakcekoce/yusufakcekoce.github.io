7193262a982bd157d62237a4e2cf0365cf1b76c8
Kayra <kayrabtuncer@gmail.com>
2021-03-11 19:23:00 +0300

    edited bio style

--- src/components/bio.js
+++ src/components/bio.js
@@ -20,29 +20,27 @@ const Bio = () => {
   `)
 
   const author = data.site.siteMetadata?.author
-  const social = data.site.siteMetadata?.social
 
   return (
     <div className="bio">
+      <h2>Who am I?</h2>
+      <div>
       <StaticImage
         className="bio-avatar"
         layout="fixed"
         formats={["AUTO", "WEBP", "AVIF"]}
         src="../images/profile-pic.png"
-        width={50}
-        height={50}
+        width={200}
+        height={200}
         quality={95}
         alt="Profile picture"
       />
       {author?.name && (
         <p>
-          Hello my name's <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://github.com/${social?.github || ``}`}>
-            GitHub
-          </a>
+          Hello my name's <strong>{author.name}</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer facilisis, diam non dictum consectetur, elit eros sagittis felis, eget laoreet sem risus nec odio. Ut suscipit quis lectus at fermentum.
         </p>
       )}
+      </div>
     </div>
   )
 }
